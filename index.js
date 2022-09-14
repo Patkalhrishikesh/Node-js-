d7deaa078ef9aa5074324e053897df30a6bbaf89
patkalhrishikesh <patkalhrishikesh16@gmail.com>
2022-09-14 11:01:27 +0530

    fundmentals

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-console.log("subject");
-var a = 20;
-var b = 30;
-var c = 40;
+const app = require('./app')
+let arr = [2,3,4,5,3,4,5,6,7,8];
+console.log(app.xyz());
 
+const result = arr.filter((item)=>{
+    console.log(item)
+    return item > 4;
+})
 
-console.log(a+b+c);
+console.warn(result);
